d5e97a6a25c7344922ae0d06c04bfce2476fd75a
Satish Singh <i.sksingh113@gmail.com>
2025-06-11 23:13:12 +0530

    api

--- routes/formRoute.js
+++ routes/formRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { submitForm } = require("../controller/form1Controller");
+const { submitForm99 } = require("../controller/form99Controller");
 const { submitForm100 } = require("../controller/form100controller");
 const { submitForm101 } = require("../controller/form101controller");
 const { submitForm102 } = require("../controller/form102Controller");
@@ -7,7 +7,7 @@ const { submitForm102 } = require("../controller/form102Controller");
 const checkForAuthenticationCookie = require("../authMiddleware/authMiddleware");
 const router = express.Router();
 
-router.post("/submit-form", checkForAuthenticationCookie("token"), submitForm);
+router.post("/submit-form99", checkForAuthenticationCookie("token"), submitForm99);
 router.post("/submit-form100", checkForAuthenticationCookie("token"), submitForm100);
 router.post("/submit-form101", checkForAuthenticationCookie("token"), submitForm101);
 router.post("/submit-form102", checkForAuthenticationCookie("token"), submitForm102);
